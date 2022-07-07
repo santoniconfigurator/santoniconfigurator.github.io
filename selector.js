31a830923bc31454cbc5b9ad95f2decb082eecb3
JamesMaloney <santoniconfigurator@gmail.com>
2022-07-07 12:37:35 +0200

    Direct link speed-up attempt

--- selector.js
+++ selector.js
@@ -2,10 +2,12 @@
 var url = new URL(window.location.href);
 var shoe_name = url.searchParams.get("name");
 
+var default_raw_url = "https://raw.githubusercontent.com/santoniconfigurator/santoniconfigurator.github.io/main/models/";
+
 // Find the shoe model or default to the sneaker one
 if(shoe_name != null) {
-    document.getElementById("model").setAttribute("src", "models/" + shoe_name + ".glb");
-    document.getElementById("model").setAttribute("ios-src", "models/" + shoe_name + ".usdz");
+    document.getElementById("model").setAttribute("src", default_raw_url + shoe_name + ".glb");
+    document.getElementById("model").setAttribute("ios-src", default_raw_url + shoe_name + ".usdz");
 }
 else {
     document.getElementById("model").setAttribute("src", "models/sneaker.glb");
